872786ed3af50933da083e537fadf19b2c3e75ed
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-06-17 19:30:02 +0200

    Fix: ReactDOM.render is no longer supported in React 18
    React 18 introduces a new root API which provides better ergonomics for managing roots.
    
    https://react.dev/blog/2022/03/08/react-18-upgrade-guide#updates-to-client-rendering-apis

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import App from "./App";
+import "./index.css";
+
+const CONTAINER = document.getElementById("app");
+const ROOT = createRoot(CONTAINER);
+
+ROOT.render(<App />);
